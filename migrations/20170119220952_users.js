90e73958760518dfb852a1240045e454691ea327
Afanasev Dmitriy <afanavt@gmail.com>
2017-01-20 00:01:00 +0700

    сохранил пользователя в базу данных

---
+++ migrations/20170119220952_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.string('username').notNullable().unique();
+    table.string('email').notNullable().unique();
+    table.string('timezone').notNullable();
+    table.string('password_diggest').notNullable();
+    table.timestamps();
+  });  
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable();
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import validateInput from '../shared/validations/signup';
+
+import User from '../models/user';
 
 let router = express.Router();
 
@@ -7,7 +10,22 @@ router.post('/', (req, res) => {
   const {errors, isValid} = validateInput(req.body);
 
   if(isValid){
-    res.json({ success: true });
+    const {username, timezone, email, password} = req.body;
+    const password_diggest = bcrypt.hashSync(password, 10);
+
+    User.forge({ 
+      username, timezone, email, password_diggest
+    }, {
+      hasTimestamps: true
+    })
+      .save()
+      .then(user => res.json({
+        success: true
+      }))
+      .catch(err => res.status(500).json({
+        error: err
+      }));
+
   } else {
     res.status(400).json(errors);
   }
